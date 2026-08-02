e8f9760f6c15da50dea520b05793f8c57088f465
Dertyuggg <malligapmtc@gmail.com>
2026-08-02 22:36:52 +0530

    api

--- api/sensors.js
+++ api/sensors.js
@@ -1,15 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
+
     process.env.https://hoirvpryemdhqsxomlnu.supabase.co,
+
     process.env.sb_secret_Ca9zntEFLazDzpHDaz_3uw_toPKCW9W_SERVICE_ROLE_KEY
+
 );
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
-        return res.status(405).json({
-            error: "Method not allowed"
-        });
+        return res.status(405).json({ error: "Method not allowed" });
     }
 
     try {
@@ -26,9 +27,7 @@ export default async function handler(req, res) {
             .select();
 
         if (error) {
-            return res.status(500).json({
-                error: error.message
-            });
+            return res.status(500).json({ error: error.message });
         }
 
         return res.status(200).json({
